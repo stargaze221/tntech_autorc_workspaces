d719a6c548ad3ebe8566a03437fc2d627ad09131
Hyung-Jin Yoon <stargaze221@gmail.com>
2025-07-20 12:20:32 -0500

    rc_bridge added

--- isaac_ros-dev/src/vehicle_control_unit/setup.py
+++ isaac_ros-dev/src/vehicle_control_unit/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'vehicle_control_unit'
 
@@ -10,16 +12,20 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='yoonlab02agxcar',
-    maintainer_email='yoonlab02agxcar@todo.todo',
+    maintainer='autovhc',
+    maintainer_email='autovhc@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'rc_bridge_node = vehicle_control_unit.rc_bridge_node:main',
+            
         ],
     },
 )
